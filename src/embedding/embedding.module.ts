ecf3d56d0d16868edb20e919d01f706d814fa7ea
Muhammad Talha <13951043+TalhaMaliktz@users.noreply.github.com>
2026-08-22 17:52:42 +0500

    Import OllamaEmbedProvider to the Embed module

--- src/embedding/embedding.module.ts
+++ src/embedding/embedding.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GeminiEmbeddingProvider } from './providers/gemini.provider';
+import { OllamaEmbeddingProvider } from './providers/ollama.provider';
 import { EmbeddingFactory } from './embedding.factory';
 
 @Module({
-    providers: [GeminiEmbeddingProvider, EmbeddingFactory],
+    providers: [GeminiEmbeddingProvider, OllamaEmbeddingProvider, EmbeddingFactory],
     exports: [EmbeddingFactory],
 })
 export class EmbeddingModule { }
